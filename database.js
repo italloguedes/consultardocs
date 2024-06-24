50be72e1bf78561b27a5317590bda382796d3718
Itallo Guedes <146380080+italloguedes@users.noreply.github.com>
2024-06-24 23:51:58 +0000

    new att 2

--- database.js
+++ database.js
@@ -1,5 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.sqlite'); // Usando um arquivo físico
+const path = require('path');
+
+// Use o caminho absoluto para o arquivo do banco de dados
+const dbPath = path.resolve(__dirname, 'database.sqlite');
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users (
