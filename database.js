64932a784404f8d7747b8c74f5afa72cd78614be
Itallo Guedes <146380080+italloguedes@users.noreply.github.com>
2024-06-24 23:47:22 +0000

    new att

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./database.sqlite'); // Use um arquivo físico
+const db = new sqlite3.Database('./database.sqlite'); // Usando um arquivo físico
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users (
