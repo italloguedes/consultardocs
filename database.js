688c52de6fc9f8d494d8092fa92107844d8549a4
Itallo Guedes <146380080+italloguedes@users.noreply.github.com>
2024-06-25 00:26:49 +0000

    nova atualizacao de db

--- database.js
+++ database.js
@@ -1,18 +1,11 @@
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
-
-// Caminho para o arquivo do banco de dados SQLite
-const dbPath = path.resolve(__dirname, 'database.sqlite');
-
-// Cria uma instância do banco de dados SQLite
-const db = new sqlite3.Database(dbPath);
-
-// Cria a tabela 'users' se ela ainda não existir
-db.serialize(() => {
-  db.run(`CREATE TABLE IF NOT EXISTS users (
-    cpf TEXT PRIMARY KEY,
-    name TEXT NOT NULL
-  )`);
-});
-
-module.exports = db;
+{
+    "version": 2,
+    "builds": [
+      { "src": "index.js", "use": "@vercel/node" }
+    ],
+    "env": {
+      "DATABASE_URL": "postgresql://cins_owner:******@ep-holy-dawn-a5rmvw2m-pooler.us-east-2.aws.neon.tech/cins?sslmode=require",
+      "DATABASE_URL_UNPOOLED": "postgresql://cins_owner:******@ep-holy-dawn-a5rmvw2m.us-east-2.aws.neon.tech/cins?sslmode=require"
+    }
+  }
+  
